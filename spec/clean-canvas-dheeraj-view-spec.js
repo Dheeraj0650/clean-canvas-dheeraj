a5177c72eb0e7bef793cc86c0452c86272fe8816
Dheeraj0650 <kottedheeraj@gmail.com>
2022-10-04 16:10:52 -0600

    Initial commit

---
+++ spec/clean-canvas-dheeraj-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CleanCanvasDheerajView from '../lib/clean-canvas-dheeraj-view';
+
+describe('CleanCanvasDheerajView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
